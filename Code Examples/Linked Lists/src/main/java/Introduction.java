a4ce93318c63214eadc2ccc0684c7467d6e915ab
jmmg2789 <jm.mondragongomez@gmail.com>
2023-01-03 11:06:08 -0600

    rough implementation of a linked list from scratch

--- Code Examples/Linked Lists/src/main/java/Introduction.java
+++ Code Examples/Linked Lists/src/main/java/Introduction.java
@@ -1,7 +1,13 @@
+
 public class Introduction {
 
-    
+    // call to a linked list
 
     public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList(10);
+        linkedList.nextNode(5);
+        linkedList.nextNode(16);
+
+        System.out.println(linkedList.toString());
     }
 }
