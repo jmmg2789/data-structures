ea9de764892bf2baa60d62ee4180de3d36e1658f
jmmg2789 <jm.mondragongomez@gmail.com>
2023-01-03 23:53:24 -0600

    add remove function to tree

--- Code Examples/Trees/src/main/java/BinarySearchTree.java
+++ Code Examples/Trees/src/main/java/BinarySearchTree.java
@@ -67,6 +67,88 @@ public class BinarySearchTree {
         return null;
     }
 
+    public boolean remove(Integer value) {
+        if (this.root == null) {
+            return false;
+        }
+
+        Node currentNode = this.root;
+        Node parentNode = null;
+
+        while (currentNode != null) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value.equals(value)) {
+                // we find a match
+
+                // if there is no right child
+                if (currentNode.right == null) {
+                    if (parentNode == null) {
+                        this.root = currentNode.left;
+                    } else {
+                        // if the parent > current value, make the current left child a child of the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                        }
+                        // if parent < current value, make left child a right child of parent
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                }
+                // checks if the right child that doesn't have a left child
+                else if (currentNode.right.left == null) {
+                    if (parentNode == null) {
+                        this.root = currentNode.left;
+                    } else {
+                        currentNode.right.left = currentNode.left;
+
+                        // if parent > current node, make the right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+                        }
+                        // if parent < current node, make right child a right child of the parent
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+                }
+                // if the right child has a left child
+                else {
+                    // find right child's left most child
+                    Node leftMost = currentNode.left.right;
+                    Node leftMostParent = currentNode.right;
+                    while(leftMost.left != null) {
+                        leftMostParent = leftMost;
+                        leftMost = leftMost.left;
+                    }
+
+                    // parent's left subtree is now leftmost's right subtree
+                    leftMostParent.left = leftMost.right;
+                    leftMost.left = currentNode.left;
+                    leftMost.right = currentNode.right;
+
+                    if (parentNode == null) {
+                        this.root = leftMost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftMost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftMost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(9);
