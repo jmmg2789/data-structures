312144cdc9830f35bae15b67814f0fc8be6b433e
jmmg2789 <jm.mondragongomez@gmail.com>
2023-01-03 20:35:00 -0600

    stacks and queues sandbox

---
+++ Code Examples/Stacks and Queues/src/main/java/StackExample.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class StackExample {
+
+    Stack<String> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        StackExample stackExample = new StackExample();
+
+        stackExample.stack.push("Hello");
+        stackExample.stack.push("my");
+        stackExample.stack.push("name");
+        stackExample.stack.push("is");
+        stackExample.stack.push("Jose");
+
+        System.out.println(stackExample.stack);
+        System.out.println(stackExample.stack.peek());
+
+        stackExample.stack.push("ya heard");
+        System.out.println(stackExample.stack);
+
+        stackExample.stack.pop();
+        System.out.println(stackExample.stack.peek());
+        
+    }
+
+}
