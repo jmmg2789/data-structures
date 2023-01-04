32bb0bd9523b692743034b7f5f09197275645e8c
jmmg2789 <jm.mondragongomez@gmail.com>
2023-01-03 20:51:12 -0600

    more experimenting

--- Code Examples/Stacks and Queues/src/main/java/QueueExample.java
+++ Code Examples/Stacks and Queues/src/main/java/QueueExample.java
@@ -4,19 +4,42 @@ public class QueueExample {
 
     LinkedList<String> queue = new LinkedList<>();
 
+    public String peek() {
+        System.out.println(queue.get(0));
+        return queue.get(0);
+    }
+
+    public void enqueue(String item) {
+        queue.add(item);
+        System.out.println(item + " successfully queued.");
+    }
+
+    public void dequeue() {
+        queue.removeFirst();
+        System.out.println("Successfully removed from queue.");
+    }
+
     public static void main(String[] args) {
 
         QueueExample ex = new QueueExample();
 
-        ex.queue.add("my");
-        ex.queue.add("name");
-        ex.queue.add("is");
-        ex.queue.add("Jose");
+//        ex.queue.add("my");
+//        ex.queue.add("name");
+//        ex.queue.add("is");
+//        ex.queue.add("Jose");
+//
+//        System.out.println(ex.queue);
+//        ex.queue.addFirst("Hello");
+//        System.out.println(ex.queue);
+//        System.out.println(ex.queue.getFirst());
 
+        ex.enqueue("Ya boi");
+        ex.peek();
+        ex.enqueue("Ya girl");
+        ex.peek();
         System.out.println(ex.queue);
-        ex.queue.addFirst("Hello");
-        System.out.println(ex.queue);
-        System.out.println(ex.queue.getFirst());
+        ex.dequeue();
+        ex.peek();
 
     }
 
